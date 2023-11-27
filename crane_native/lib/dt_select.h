18dfd9941d34ab1a36d452e8dc976fff3fe00a49
= <=>
2023-11-27 21:58:26 +0200

    added custom data formats

--- crane_native/lib/dt_select.h
+++ crane_native/lib/dt_select.h
@@ -1,14 +1,21 @@
 #ifndef DT_SELECT_H
 #define DT_SELECT_H
 #include <stdio.h>
+#include <stdlib.h>
 #include "err.h"
+#include "wrepl.h"
 
-char* set_data_format(char* name[])
+char* set_data_format(const char* name)
 {
+    char* dr = malloc(256);
+    if (name == NULL) return NULL;
+    if (dr == NULL) return NULL;
+    strcpy(dr,name);
     if (name != NULL)
-    {
-        FILE* data_format = fopen("crane_native/data/format.name","w");
-        fclose(data_format);
+    {   
+        replace_string("crane_native/data/format.name","name",dr);
+        free(dr);
+        dr = NULL;
     }
     else
     {
