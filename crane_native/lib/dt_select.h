06aafd6b26276c19c5ed7d63b3fbbe9520d017b1
= <=>
2023-11-28 22:23:54 +0200

    settings data format to global

--- crane_native/lib/dt_select.h
+++ crane_native/lib/dt_select.h
@@ -2,28 +2,40 @@
 #define DT_SELECT_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "err.h"
 #include "wrepl.h"
+#include "data_format.h"
 
-char* set_data_format(const char* name)
+char* set_data_format(char* fname)
 {
-    char* dr = malloc(256);
-    if (name == NULL) return NULL;
-    if (dr == NULL) return NULL;
-    strcpy(dr,name);
-    if (name != NULL)
+    if (fname == NULL) return NULL;
+    if (fname != NULL)
     {   
-        const char* ffp = "crane_native/data/format.name";
-        char* crp = replace_string(ffp,"name",dr);
-        FILE* form;
-        form = fopen(crp,"w");
-        printf("%s",crp);
-        free(dr);
-        dr = NULL;
+        strcpy(data_format_name,fname);
+        printf("%s",data_format_name);
     }
     else
     {
         arg_err("set_data_format");
+    }
+}
+
+char* create_data_file(const char* name)
+{
+    char* dr = malloc(256);
+    const char* ffp = "crane_native/data/data.name";
+    if (name == NULL) return NULL;
+    if (dr == NULL) return NULL;
+
+    strcpy(dr,name);
+    if (name != NULL)
+    {
+        char* crp = replace_string(ffp,"name",dr);
+        FILE* form;
+        form = fopen(crp,"w");
+        free(dr);
+        dr = NULL;
     }
 }
 
